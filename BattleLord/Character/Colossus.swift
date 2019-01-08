7c26db0d47f5c6295f949de3810e6f4c6425ce07
janinjan <janin.culhaoglu@gmail.com>
2019-01-09 00:22:58 +0100

    Overriding attack() method for Colossus

--- BattleLord/Character/Colossus.swift
+++ BattleLord/Character/Colossus.swift
@@ -14,5 +14,22 @@ class Colossus: Character {
     init(name: String) {
         super.init(characterType: "Colossus", lifePoint: 130, weapon: Punch(), name: name)
     }
-
+    
+    // Override attack method for Colossus
+    override func attack(characterToAttack: Character) {
+        switch weapon {
+        case is Punch:
+            super.attack(characterToAttack: characterToAttack) // Attack method from parent class
+            characterToAttack.checkLifeCharacter() // if character's life under 0 set it to 0 with dead message
+        case is TheServantOfTheLord: // Override attack method
+            characterToAttack.lifePoint -= weapon.weaponsDamage
+            print("Your enemy \(characterToAttack.name) has lost \(weapon.weaponsDamage) life points")
+            self.lifePoint -= weapon.weaponsAftermath // by using this weapon, Colossus looses life points due to aftermath
+            print("Using \(weapon.weaponsName) decreases \(weapon.weaponsAftermath) life points on self due to aftermath")
+            characterToAttack.checkLifeCharacter()
+            self.checkLifeCharacter()
+        default:
+            break
+        }
+    }
 }
