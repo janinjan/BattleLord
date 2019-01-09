0751c3730cbb812e465d8d1b0d95c558642062e6
janinjan <janin.culhaoglu@gmail.com>
2019-01-09 21:17:11 +0100

    Created TeamFactory class, include method to control that name is unique in game

---
+++ BattleLord/Team/Team.swift
@@ -0,0 +1,95 @@
+//
+//  Team.swift
+//  BattleLord
+//
+//  Created by Janin Culhaoglu on 17/12/2018.
+//  Copyright © 2018 Janin Culhaoglu. All rights reserved.
+//
+
+import Foundation
+
+class Team {
+    
+    //MARK: - Properties
+    var characters = [Character]()
+    var teamName = ""
+    
+    
+    //MARK: - Init
+    init() {
+        createTeamName()
+        createCharacters()
+    }
+    
+    
+    //MARK: - Methods
+    
+    // The player enters name for his team
+    func createTeamName() {
+        repeat {
+            if let data = readLine() {
+                teamName = data
+            }
+        } while teamName == ""
+    }
+    
+    // Show characters in team with their descriptions
+    func showCharactersInTeam() {
+        print("\n 🗡 Team \(teamName):")
+        for character in characters {
+            character.describeCharacter()
+        }
+    }
+    
+    // First Interface
+    func menuListOfCharacter() {
+        print("\n ============================================"
+            + "\n Add 3 characters to your Team :"
+            + "\n1. Fighter   - Life: 100   - Damages : 10 pts"
+            + "\n2. Magus     - Life:  60   - Heal    : 30 pts"
+            + "\n3. Colossus  - Life: 130   - Damages :  5 pts"
+            + "\n4. Dwarf     - Life:  45   - Damages : 20 pts"
+            + "\n5. Thief     - Life:  70   - Damages : 15 pts"
+            + "\n ============================================"
+            + "\n Select a number between 1 and 5 :")
+    }
+    
+    // Create 3 Characters
+    private func createCharacters() {
+        for _ in 0..<3 {
+            createCharacter()
+        }
+    }
+    
+    // The player creates a character to add to his Team
+    func createCharacter() {
+        var userChoice = 0
+        menuListOfCharacter() // Show list of characters available to compose a team
+        TeamFactory.enterNumber(userChoice: &userChoice) // Player enters number to select Character from the list
+        let uniqueName = TeamFactory.uniqueCharacterName() // Add character's name and check if it is unique in game
+        switch userChoice {
+        case 1:
+            let fighter = Fighter(name: uniqueName)
+            characters.append(fighter) // Add a Fighter
+            print("A Fighter named \(fighter.name) is added !")
+        case 2:
+            let magus = Magus(name: uniqueName)
+            characters.append(magus) // Add a Magus
+            print("A Magus named \(magus.name) is added !")
+        case 3:
+            let colossus = Colossus(name: uniqueName)
+            characters.append(colossus) // Add a Colossus
+            print("A Colossus named \(colossus.name) is added !")
+        case 4:
+            let dwarf = Dwarf(name: uniqueName)
+            characters.append(dwarf) // Add a Dwarf
+            print("A Dwarf named \(dwarf.name) is added !")
+        case 5:
+            let thief = Thief(name: uniqueName)
+            characters.append(thief) // Add a Thief
+            print("A Thief named \(thief.name) is added !")
+        default:
+            break
+        }
+    }
+}
---
+++ BattleLord/Team/TeamFactory.swift
@@ -0,0 +1,46 @@
+//
+//  TeamFactory.swift
+//  BattleLord
+//
+//  Created by Janin Culhaoglu on 04/01/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import Foundation
+
+class TeamFactory {
+    
+    //MARK: - Property
+    private static var charactersName = [String]()
+    
+    // Check that entered name is unique in the Game
+    static func uniqueCharacterName() -> String {
+        var characterName = ""
+        repeat {
+            print("Enter a name for your character :")
+            if let data = readLine() {
+                characterName = data
+                if charactersName.contains(characterName) {
+                    print("This name already exist !")
+                    characterName = ""
+                } else {
+                    charactersName.append(characterName)
+                }
+            }
+        } while characterName == ""
+        return characterName
+    }
+    
+    // Player's choice  1, 2, 3, 4 or 5 to select Character
+    static func enterNumber(userChoice: inout Int) {
+        repeat {
+            if let data = readLine() {
+                if let dataToInt = Int(data) {
+                    userChoice = dataToInt
+                }
+            }
+        } while userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4 && userChoice != 5
+    }
+}
+
+
